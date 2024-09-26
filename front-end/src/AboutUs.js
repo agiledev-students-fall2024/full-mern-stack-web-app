8f80f740eb76ecaf1eb37b755cc3dfa75f2d2028
qq3173732005 <76537620+qq3173732005@users.noreply.github.com>
2024-09-26 06:35:44 -0400

    about us page with text

---
+++ front-end/src/AboutUs.js
@@ -0,0 +1,20 @@
+import './AboutUs.css'
+
+/**
+ * A React component that represents the Home page of the app.
+ * @param {*} param0 an object holding any props passed to this component from its parent component
+ * @returns The contents of this component, in JSX form.
+ */
+const AboutUs = props => {
+  return (
+    <>
+      <h1>About us page</h1>
+      <p>
+        about us
+      </p>
+    </>
+  )
+}
+
+// make this component available to be imported into any other file
+export default AboutUs
